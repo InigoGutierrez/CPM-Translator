0501aefb71df74d91ad76da4ec8bf30c36e9df1d
InigoGutierrez <inigogf.95@gmail.com>
2017-10-28 19:20:27 +0200

    Solved Exercise 5 (save changes name label of original document)

--- Practica7/src/igu/VentanaPrincipal.java
+++ Practica7/src/igu/VentanaPrincipal.java
@@ -1,18 +1,33 @@
 package igu;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -98,6 +113,12 @@ public class VentanaPrincipal extends JFrame {
 	private JButton getBtGuardar() {
 		if (btGuardar == null) {
 			btGuardar = new JButton();
+			btGuardar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					String name = JOptionPane.showInputDialog("Please, input the name of the original document:");
+					etIdOriginal.setText(name);
+				}
+			});
 			btGuardar.addMouseListener(exListener);
 			btGuardar.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/img/Guardar.png")));
 			btGuardar.setBorderPainted(false);
